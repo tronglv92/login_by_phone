939ec77e0fbb9c7869a78568ff987611d0084c54
trong <trong.luong270292@gmail.com>
2021-06-06 11:41:13 +0700

    fix bug build

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -15,7 +15,7 @@ export PATH=`pwd`/flutter/bin:$PATH
 
 flutter channel stable
 flutter doctor
-flutter build apk --release
+flutter build apk --release --flavor appCenter
 
 #copy the APK where AppCenter will find it
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/flutter-apk/app-appcenter-release.apk $_
