5c9df918ba10d1faf801f71501e6faf31d1a4a78
trong <trong.luong270292@gmail.com>
2021-06-06 14:22:04 +0700

    fix bugs

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -6,7 +6,9 @@ sudo gem uninstall cocoapods --all --executables
 COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock`
 echo "Installing CocoaPods version $COCOAPODS_VER"
 sudo gem install cocoapods -v $COCOAPODS_VER
-
+rm Podfile.lock
+pod setup
+pod install
 # fail if any command fails
 set -e
 # debug log
