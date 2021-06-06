098a3ff510fb550eaa35d628251edcb4ef54bdf2
trong <trong.luong270292@gmail.com>
2021-06-06 15:01:14 +0700

    fix

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 #Place this script in project/ios/
-#echo "Uninstalling all CocoaPods versions"
-#sudo gem uninstall cocoapods --all --executables
-#
-#COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock`
-#
-#echo "Installing CocoaPods version $COCOAPODS_VER"
-#sudo gem install cocoapods -v $COCOAPODS_VER
+echo "Uninstalling all CocoaPods versions"
+sudo gem uninstall cocoapods --all --executables
+
+COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock`
+
+echo "Installing CocoaPods version $COCOAPODS_VER"
+sudo gem install cocoapods -v $COCOAPODS_VER
 
 # fail if any command fails
 set -e
 # debug log
 set -x
-#pod setup
+pod setup
 
 cd ..
 git clone -b beta https://github.com/flutter/flutter.git
@@ -24,4 +24,4 @@ flutter pub get
 
 echo "Installed flutter to `pwd`/flutter"
 
-flutter build ios --release --no-codesign
+#flutter build ios --release --no-codesign
