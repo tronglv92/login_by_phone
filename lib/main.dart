8c8f8b147487493ee3ed9682116da3ea9165ce29
trong <trong.luong270292@gmail.com>
2021-06-08 15:50:11 +0700

    fix for build bitrise

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'dart:isolate';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_appcenter_bundle/flutter_appcenter_bundle.dart';
+
 import 'package:login_by_phone/utils/app_config.dart';
 
 import 'package:login_by_phone/utils/app_theme.dart';
@@ -16,12 +16,7 @@ import 'my_app.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   AppConfig(env: Env.dev(), theme: AppTheme.origin());
-  await AppCenter.startAsync(
-    appSecretAndroid: '3ec2780e-24f8-4cde-a043-04aa7fc155b1',
-    appSecretIOS: '2da3d93f-6b3f-48f9-920f-2d63ae3cd25a',
-    enableDistribute: false,
-  );
-  await AppCenter.configureDistributeDebugAsync(enabled: false);
+
   await Firebase.initializeApp();
   // Initialize Crash report
   await FirebaseCrashlytics.instance
